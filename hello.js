eefe24cabdaed9d72be4c9d5e66a852a09766514
ksolomon7 <kalkidan.solomon2@gmail.com>
2021-05-31 12:13:51 -0400

    adds first node script

---
+++ hello.js
@@ -0,0 +1,9 @@
+//console.log is not part of the javascript language 
+//it is part of the runtime because it depends on the browser
+//or on node
+//This uses the global console. 
+console.log("Welcome to the class!")
+
+
+//running node on the terminal-
+//commands node filename.js or node filename
