22e86f53ba6ff9c22b8c3063ad9f6a1fc2a271e4
ksolomon7 <kalkidan.solomon2@gmail.com>
2021-06-01 19:47:24 -0400

    removes getNotes function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,6 @@
 const fs=require('fs')
 const chalk=require('chalk')
 const { showCompletionScript } = require('yargs')
-const getNotes=()=>{
-    return "Your notes..."
-}
 
 const addNote=(title, body)=>{
    const notes=loadNotes() 
