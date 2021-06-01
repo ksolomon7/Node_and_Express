5a62c8125438e5d91ac45ef3e6201f4bc6bb5879
ksolomon7 <kalkidan.solomon2@gmail.com>
2021-06-01 10:23:43 -0400

    adds process.argv learning to project

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,5 +4,7 @@ const getNotes=require('./notes')
 const notes=getNotes()
 console.log(notes);
 
-const successMsg=chalk.green("Success!")
+const successMsg=chalk.blue.inverse.bold("Success")
 console.log(successMsg);
+
+console.log(process.argv[2])
