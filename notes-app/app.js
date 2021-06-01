472f232e35393d672cd4a68dfaffb67fa138c74a
ksolomon7 <kalkidan.solomon2@gmail.com>
2021-06-01 12:06:32 -0400

    adds commands for yargs library

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,44 @@
 const chalk=require('chalk')
+const yargs=require('yargs')
 const getNotes=require('./notes')
 
-const notes=getNotes()
-console.log(notes);
+//customize yargs version
+yargs.version('1.1.0')
 
-const successMsg=chalk.blue.inverse.bold("Success")
-console.log(successMsg);
+//add, retrieve, read and list
+//create add command
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler:function (){
+        console.log("Adding a new note")
+    }
+})
 
-console.log(process.argv[2])
+//create remove command
+yargs.command({
+    command:'remove',
+    describe:"Remove a note",
+    handler:function (){
+        console.log("Removing a note")
+    }
+})
+
+//Create read command
+yargs.command({
+    command:'read',
+    describe:'Read a note',
+    handler:function(){
+        console.log("Reading a note")
+    }
+})
+
+//Create a list command
+yargs.command({
+    command:"list",
+    describe:"List your notes",
+    handler:function(){
+        console.log("Listing a note")
+    }
+})
+console.log(yargs.argv);
